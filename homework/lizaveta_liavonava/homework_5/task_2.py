5ce1d0de8e94d4060364448743e3d74b936b7a11
Lizaveta Liavonava <lizaveta.liavonava2002@gmail.com>
2026-05-07 15:13:27 +0300

    Fixed  whitespace errors

--- homework/lizaveta_liavonava/homework_5/task_2.py
+++ homework/lizaveta_liavonava/homework_5/task_2.py
@@ -1,9 +1,9 @@
-text='результат операции: 42'
-digit=int(text[text.index(':') + 2:])
+text ='результат операции: 42'
+digit = int(text[text.index(':') + 2:])
 print(digit + 10)
-text='результат операции: 514'
-digit=int(text[text.index(':') + 2:])
+text = 'результат операции: 514'
+digit = int(text[text.index(':') + 2:])
 print(digit + 10)
-text='результат работы программы: 9'
-digit=int(text[text.index(':') + 2:])
+text = 'результат работы программы: 9'
+digit = int(text[text.index(':') + 2:])
 print(digit + 10)
