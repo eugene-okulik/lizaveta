5f2baeb2c655247ec7b2699cfda64dae52d52c5a
Lizaveta Liavonova <lliavonova@MBP-lliavonava.local>
2026-09-22 15:19:26 +0300

    lint fixed

--- homework/lizaveta_liavonava/homework_16/task_1.py
+++ homework/lizaveta_liavonava/homework_16/task_1.py
@@ -77,7 +77,7 @@ with (open(file_path_1, newline='', encoding='utf-8') as csv_file):
                     and row_csv['subject_title'] == row_db['subject_title']
                     and row_csv['lesson_title'] == row_db['lesson_title']
                     and row_csv['mark_value'] == str(row_db['mark'])
-                ):
+            ):
                 break
         else:
             print('Data not found:', row_csv)
